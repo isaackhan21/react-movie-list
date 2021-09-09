fb866c045e72e929143f32eb8bdfdae61917aa88
Awesomecoder26 <isaackhan21@gmail.com>
2021-09-09 14:23:42 +0100

    Minor changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Base from "./components/Base";
 
-const url = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=35`;
+const url =
+  "https://api.themoviedb.org/3/discover/movie?api_key=4d4fa02f74f80466555c39a034ecfb35&with_genres=35";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
